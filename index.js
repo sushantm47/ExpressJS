af42d94e9892bb185c644cac6815807e8a22fa60
Sushant <vettel@pop-os.localdomain>
2020-02-01 00:05:11 +0530

    48 mins 1 err at mesages (commented)

--- index.js
+++ index.js
@@ -1,29 +1,20 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const moment = require("moment");
-const members = require("./members.js");
-
+const logger = require("./Middleware/logger");
 // app.get("/", (req, res) => {
 //   res.send("<h1>HELLO!</h1>");
 // });
 
-const logger = (req, res, next) => {
-  console.log(
-    `${req.protocol}://${req.get("host")}${
-      req.originalUrl
-    }:${moment().format()}`
-  );
-  next();
-};
+///app.use(logger);
 
-app.use(logger);
+//Body Parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.get("/api/members", (req, res) => {
-  res.json(members);
-});
+app.use(express.static(path.join(__dirname, "public")));
 
-// app.use(express.static(path.join(__dirname, "public")));
+app.use("/api/members", require("./routes/api/members"));
 
 const PORT = process.env.PORT || 5000;
 
